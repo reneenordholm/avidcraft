009f491ef668843d9f4d650c0a20c5ea75bca178
Renee Nordholm <reneenordholm@gmail.com>
2019-08-08 13:41:48 -0500

    index and layout functioning

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,15 @@ require './config/environment'
 
 class ApplicationController < Sinatra::Base
 
-    # configure do
-    #     set :public_folder, 'public'
-    #     set :views, 'app/views'
-    #     enable :sessions
-    #     set :session_secret, "secret"
-    #   end
+    configure do
+        set :public_folder, 'public'
+        set :views, 'app/views'
+        enable :sessions
+        set :session_secret, "secret"
+      end
 
     get '/' do
-        "Hello World"
+        
+        erb :'/index'
     end
 end
