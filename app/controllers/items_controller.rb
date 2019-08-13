2a574a51ce279a731958704c0fa2d50567ad0db3
Renee Nordholm <reneenordholm@gmail.com>
2019-08-13 15:55:31 -0500

    fields hold value should errors trigger

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 class ItemsController < ApplicationController
 
   # items index
@@ -23,7 +21,7 @@ class ItemsController < ApplicationController
     # does not let user view new item page if not logged in
     redirect '/' if !logged_in?
 
-    # set class variable for @item to new Item instance so erb can check for errors
+    # set instance variable for @item to new Item instance so erb can check for errors
     @item = Item.new
 
     erb :"/store/new_item"
@@ -32,7 +30,8 @@ class ItemsController < ApplicationController
   # create
   post '/items' do
     # item id is set as belonging to logged in user
-    # item instance is created and saved to db
+    # item instance is created and saved as object in db
+    # item set to @item instance variable so errors can be generated, if arise
     @item = Item.create(title: params[:title], description: params[:description], price: params[:price], user: current_user, image: params[:image])
       
     # if any validations fail upon item creation raises an error in erb
@@ -41,72 +40,76 @@ class ItemsController < ApplicationController
       # presents create_user page again, uses cookie to show shows account creation errors
       erb :"/store/new_item"
     else
-      # successful item object created, redirects user to list of all items
-      redirect '/items'
+      # successful item object created, redirects user to list of all items created by user
+      redirect '/items/:id'
     end
   end
 
-    # read
-    get '/items/:id' do
-        # logged in user can view a single item
-        # logged out user cannot view a single item
-        if logged_in?
-            # sets item id to instance variable so that items's data can be viewed
-            @item = Item.find_by_id(params[:id])
+  # read
+  get '/items/:id' do
+    # if not logged in redirects user to home page
+    # logged in user can view a single item
+    # logged out user cannot view a single item
+    redirect '/' if !logged_in?
 
-            erb :"store/show_item"
-        else
-            # if not logged in redirects user to home page
-            redirect to '/'
-        end
+    # sets item id to instance variable so that items's data can be viewed in erb
+    @item = Item.find_by_id(params[:id])
+
+    erb :"store/show_item"
+  end
+
+  # update
+  get '/items/:id/edit' do
+    # sets item id to instance variable to that item's data can be viewed/edited
+    @item = Item.find_by(id: params[:id])
+
+    # user cannot edit an item they did not create
+    # user can edit their own item if logged in
+    if logged_in? && @item.user_id == current_user.id
+
+      #renders edit item form
+      erb :"store/edit_item"
+    else
+
+      # redirects to index if user not logged in/not user's item
+      redirect '/'
     end
+  end
 
-    # update
-    get '/items/:id/edit' do
-        # sets item id to instance variable to that item's data can be viewed/edited
-        @item = Item.find_by(id: params[:id])
-
-        # lets a user use item edit form if logged in
-        # does not let a user edit a item they did not create
-        # if user is logged in can edit their own item
-        if logged_in? && @item.user_id == current_user.id
-            erb :"store/edit_item"
-        else
-            # does not load edit form if user not logged in, redirects to home page
-            redirect '/'
-        end
-    end
-
-    # update
-    patch '/items/:id' do
-        # sets item id to instance variable so that items's data can be viewed/edited
-        item = Item.find_by(id: params[:id])
+  # update
+  patch '/items/:id' do
+    # sets item id to instance variable so that items's data can be viewed/edited
+    @item = Item.find_by(id: params[:id])
     
-        # does not let user edit text with blank content
-        if !params[:title].empty? && !params[:description].empty? && !params[:price].empty? && !params[:image].empty?
-            # update/saves edited item and sends back to that items's page
-            item.update(title: params[:title], description: params[:description], price: params[:price], image: params[:image])
-            redirect to "/items/#{item.id}"
-        else
-            # if params empty redirects to edit form
-            redirect to "/items/#{item.id}/edit"
-        end
-    end
+    # does not let user edit text with blank content
+    # update/saves edited item and sends back to that items's page
+    @item.update(title: params[:title], description: params[:description], price: params[:price], image: params[:image])
+            
+    # if any validations fail upon item edit raises an error in erb
+    if @item.errors.any?
 
-    # delete
-    delete '/items/:id' do
-        # sets item id to instance variable to that item's data can be viewed/edited
-        item = Item.find_by_id(params[:id])
-    
-        # allows user to delete item if logged in
-        # does not let user delete item they did not create
-        if item.user_id == current_user.id
-            item.delete
-        end
-    
-        # send user to item index after deletion
-        # does not load/let user delete item if not logged in
-        redirect '/items'
+      # presents edit item page again with errors listed
+      erb :"/store/edit_item"
+    else
+            
+      redirect '/items/#{@item.id}'
     end
+  end
+
+  # delete
+  delete '/items/:id' do
+    # sets item id to instance variable to that item's data can be viewed/edited
+    item = Item.find_by_id(params[:id])
+    
+    # allows user to delete item if logged in
+    # does not let user delete item they did not create
+    if item.user_id == current_user.id
+      item.delete
+    end
+    
+    # send user to item index after deletion
+    # does not load/let user delete item if not logged in
+    redirect '/items'
+  end
 
 end
