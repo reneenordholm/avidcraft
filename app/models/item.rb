1aea78673b71514d49674dde289df1cb66e122eb
Renee Nordholm <reneenordholm@gmail.com>
2019-08-13 17:00:24 -0500

    item spec update

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,13 @@
 class Item < ActiveRecord::Base
     belongs_to :user
 
-    # does not let user create a blank item or leave any fields blank
+    # Title can't be blank
+    # Description can't be blank
+    # Price can't be blank
+    # Price must be in x.xx format
+    # Image can't be blank
+    # Image URL must begin with http or https and end with .gif, .jpg or .png
+    # validations work with editing an item as well
     validates :title, presence: true
     validates :description, presence: true
     validates :price, presence: true, format: { with: /(^[+-]?[0-9]{1,3}(?:,?[0-9]{3})*\.[0-9]{2}$)/, message: "must be in x.xx format" }
